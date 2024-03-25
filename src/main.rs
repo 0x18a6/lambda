04052539ebe87bf39d0482d8d393cf54effe609b
0x18a6 <huffcbytecode@gmail.com>
2024-03-25 19:46:44 +0100

    feat: cleaner solution

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
 use lambdaworks_math::{
     cyclic_group::IsGroup,
     elliptic_curve::{
-        short_weierstrass::{
-            curves::bls12_381::curve::BLS12381Curve,
-            point::{Endianness, PointFormat},
-        }, traits::IsEllipticCurve
+        short_weierstrass::curves::bls12_381::curve::BLS12381Curve,
+        traits::IsEllipticCurve,
     },
 };
-use hex;
 
 fn main() {
     let secret_key: u64 = 0x6C616D6264617370;
-
-    // public key computation
     let public_key = BLS12381Curve::generator().operate_with_self(secret_key);
-
-    // public key serialization
-    // see: https://github.com/lambdaclass/lambdaworks/blob/019cfcfdc82ee51f50be81ee5bfbb479a0482181/math/src/elliptic_curve/short_weierstrass/point.rs#L237
-    let public_key = public_key.serialize(PointFormat::Uncompressed, Endianness::BigEndian);
-
-    let public_key = hex::encode(public_key);
-    let public_key = &public_key[0..96]; // public_key is the x coordinate
-
-    println!("🔐 Public key:\n{}", public_key);
+    let public_key = public_key.to_affine();
+    println!("🔐 Public key:\n{}", public_key.x());
 }
