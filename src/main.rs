bb75d22400824385c889ecd3120dd2c9e991670a
0x18a6 <huffcbytecode@gmail.com>
2024-03-25 14:47:52 +0100

    fix: minor

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ fn main() {
     // public key computation
     let public_key = BLS12381Curve::generator().operate_with_self(secret_key);
 
-    // public key serialization, see: https://github.com/lambdaclass/lambdaworks/blob/019cfcfdc82ee51f50be81ee5bfbb479a0482181/math/src/elliptic_curve/short_weierstrass/point.rs#L237
+    // public key serialization
+    // see: https://github.com/lambdaclass/lambdaworks/blob/019cfcfdc82ee51f50be81ee5bfbb479a0482181/math/src/elliptic_curve/short_weierstrass/point.rs#L237
     let public_key = public_key.serialize(PointFormat::Projective, Endianness::BigEndian);
     let public_key = hex::encode(public_key);
 
